7b1a2e7c0c392c0f42027b143f28335ec1953245
Alex Paul <axp_alpaul@yahoo.com>
2015-06-25 22:58:11 -0400

    Take a photo or choose from camera roll

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -7,17 +7,126 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate,
+                        UITextFieldDelegate
+{
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    
+    var imagePickerController: UIImagePickerController!
+    
+    // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.topTextField.delegate = self
+        self.bottomTextField.delegate = self
+        
+        self.topTextField.text = "TOP"
+        self.bottomTextField.text = "BOTTOM"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: IBActions
+    
+    @IBAction func pickBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+        self.imagePickerController = UIImagePickerController()
+        self.imagePickerController.delegate = self
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+            && UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary)
+        {
+            var alert = UIAlertController(title: "MemeMe Photo", message: "Take Photo or Choose from Camera Roll", preferredStyle: UIAlertControllerStyle.ActionSheet)
+            
+            var actionTakePhoto = UIAlertAction(title: "Take Photo", style: UIAlertActionStyle.Default){
+                UIAlertAction in
+                self.cameraActionHandler()
+            }
+            var actionCameraRoll = UIAlertAction(title: "Camera Roll", style: UIAlertActionStyle.Default) {
+                UIAlertAction in
+                self.cameraRollActionHandler()
+            }
+            
+            var actionCancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel){
+                UIAlertAction in
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }
+            
+            alert.addAction(actionCameraRoll)
+            alert.addAction(actionTakePhoto)
+            alert.addAction(actionCancel)
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+        
+        else if !UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera){
+            
+            var alert = UIAlertController(title: "MemeMe Photo", message: "Choose From Camera Roll", preferredStyle: UIAlertControllerStyle.ActionSheet)
+            
+            var actionCameraRoll = UIAlertAction(title: "Camera Roll", style: UIAlertActionStyle.Default){
+                UIAlertAction in
+                self.cameraRollActionHandler()
+            }
+            
+            var actionCancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel){
+                UIAlertAction in
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }
+            
+            alert.addAction(actionCameraRoll)
+            alert.addAction(actionCancel)
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{ // safe unwrapping of the info[key] object
+            self.imageView.image = image
+        }
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    // MARK: UITextFieldDelegate Methods
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if self.topTextField.text == "TOP"{
+            self.topTextField.text = ""
+        }
+        if self.bottomTextField.text == "BOTTOM"{
+            self.bottomTextField.text = ""
+        }
+        
+        // TODO: Fix keyboard hiding bottom text field
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: Helper Methods
+    
+    func cameraActionHandler(){
+        self.imagePickerController.sourceType = UIImagePickerControllerSourceType.Camera
+        self.imagePickerController.allowsEditing = false
+        self.imagePickerController.mediaTypes = [kUTTypeImage] // capture still images ONLY
+        self.presentViewController(self.imagePickerController, animated: true, completion: nil)
+    }
+    
+    func cameraRollActionHandler(){
+        self.imagePickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.imagePickerController.allowsEditing = false
+        self.presentViewController(self.imagePickerController, animated: true, completion: nil)
     }
 
 
